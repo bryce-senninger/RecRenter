520dcc7a478c5626c13a3d42275c0262dffda682
Iceman <Zachary.Draper85@gmail.com>
2019-10-10 17:50:11 -0600

    pushing work, database and routes in progress

--- server/models/category.js
+++ server/models/category.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-  category: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  item: { type: Schema.ObjectId, ref: "Item" }
+  category: Array,
+  subCategory: Array,
+  item: { type: Schema.Types.ObjectId, ref: "Item" }
 });
 
 const Category = mongoose.model("Category", categorySchema);
--- server/models/item.js
+++ server/models/item.js
@@ -3,18 +3,18 @@ const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
   title: { type: String, required: true, unique: true },
-  category: { type: String, unique: true },
-  subCategory: String,
+  // category: { type: String, unique: true },
+  // subCategory: String,
   keyword: Array,
   description: String,
   location: String,
-  image: String,
+  images: Array,
   price: Number,
   isRented: { type: Boolean, default: false },
   created_at: { type: Date, default: null },
   ownerID: { type: Schema.Types.ObjectId, ref: "User" },
   renterID: { type: Schema.Types.ObjectId, ref: "User" },
-  category: { type: Schema.Types.ObjectId, ref: "Category" }
+  category: { type: String, field: "category", ref: "Category" }
 });
 
 const Item = mongoose.model("Item", itemSchema);
