4bc6049da0f9723faa76b51ca5e63f1d43cf1ec9
Bryce Senninger <bryce.senninger@gmail.com>
2019-10-15 21:07:00 -0600

    modal, form started

--- client/rec-renter/src/components/navBar/index.jsx
+++ client/rec-renter/src/components/navBar/index.jsx
@@ -3,42 +3,58 @@ import icon from "./mountain.png";
 import logo from "./logo-3.png";
 import lock from "./lock.png";
 import "../navBar/style-nav.css";
+import Modal from "../modal";
 
-function NavBar() {
-  return (
-    <ul className="nav">
-      <li className="nav-item">
-        <img width={50} height={50} src={logo}></img>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link active" href="#">
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Search
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Post
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Saved Posts
-        </a>
-      </li>
-      <li className="nav-item ml-auto">
-        {/* <img src={lock} alt="" /> */}
-        <button className="btn btn-primary pull-right">
-          Sign Up / Log In
-          {/* <img src={lock} alt="" /> */}
-        </button>
-      </li>
-    </ul>
-  );
+class NavBar extends React.Component {
+  state = { showModal: false };
+  handleShowMessageClick = () => this.setState({ showModal: true });
+  handleCloseModal = () => this.setState({ showModal: false });
+  render() {
+    return (
+      <div>
+        <ul className="nav">
+          <li className="nav-item">
+            <img width={50} height={50} src={logo}></img>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Search
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Post
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Saved Posts
+            </a>
+          </li>
+          <li className="nav-item ml-auto">
+            {/* <img src={lock} alt="" /> */}
+            <button
+              className="btn btn-primary pull-right"
+              onClick={this.handleShowMessageClick}
+            >
+              Sign Up / Log In
+              {/* <img src={lock} alt="" /> */}
+            </button>
+            {this.state.showModal ? (
+              <Modal onClose={this.handleCloseModal}>
+                Log in to Rec Renter
+              </Modal>
+            ) : null}
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
