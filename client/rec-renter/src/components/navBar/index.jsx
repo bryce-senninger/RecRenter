fc72a9e82a8a4ec33dcb87de6b8ffb6460e174ea
Lrop <Lagropner04@gmail.com>
2019-10-17 18:50:24 -0600

    Luke style and react componenets (NAV)

--- client/rec-renter/src/components/navBar/index.jsx
+++ client/rec-renter/src/components/navBar/index.jsx
@@ -11,50 +11,72 @@ class NavBar extends React.Component {
   handleCloseModal = () => this.setState({ showModal: false });
   render() {
     return (
-      <div>
+      // <div>
+      <div class="navbar-expand-lg ">
+        <button
+          class="navbar-toggler ml-auto"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navCollapse"
+          // data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          {/* &#9776; */}
+          <span class="navbar-toggler-icon">&#9776;</span>
+        </button>
         <ul className="nav">
           <li className="nav-item">
             <img width={50} height={50} src={logo}></img>
           </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="/">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Search
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Post
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Saved Posts
-            </a>
-          </li>
-          <li className="nav-item ml-auto">
-            {/* <img src={lock} alt="" /> */}
-            <button
-              className="btn btn-primary pull-right"
-              onClick={this.handleShowMessageClick}
-            >
-              Sign Up / Log In
+          <li></li>
+          <div className="collapse navbar-collapse" id="navCollapse">
+            <li className="nav-item">
+              <a className="nav-link active" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Post
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Saved Posts
+              </a>
+            </li>
+
+            <li className="nav-item ml-auto">
               {/* <img src={lock} alt="" /> */}
-            </button>
-            {this.state.showModal ? (
-              <Modal onClose={this.handleCloseModal}>
-                Log in to Rec Renter
-              </Modal>
-            ) : null}
-          </li>
+              <button
+                className="btn btn-primary pull-right"
+                onClick={this.handleShowMessageClick}
+              >
+                Sign Up / Log In
+                {/* <img src={lock} alt="" /> */}
+              </button>
+
+              {this.state.showModal ? (
+                <Modal onClose={this.handleCloseModal}>
+                  Log in to Rec Renter
+                </Modal>
+              ) : null}
+            </li>
+          </div>
         </ul>
       </div>
+      // </div>
     );
   }
 }
+
+// {document.onauxclick}
 
 export default NavBar;
