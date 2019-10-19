bcd410897c7038887e05466a53c96a47728546a0
Lrop <Lagropner04@gmail.com>
2019-10-19 11:04:58 -0600

    Luke class name changes and minor styling

--- client/rec-renter/src/components/featuredPosts/index.jsx
+++ client/rec-renter/src/components/featuredPosts/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "../featuredPosts/style-feature.css";
 
 function Featured(props) {
   return (
     <div className="featuredPosts">
-      <div className="card">
-        <div className="card-body">
+      <div className="card-two">
+        <div className="card-body-two">
           <h5 className="card-title">Featured Post Card</h5>
           <p className="card-text">Featured Post 1</p>
+          <div className="feature"></div>
         </div>
       </div>
     </div>
